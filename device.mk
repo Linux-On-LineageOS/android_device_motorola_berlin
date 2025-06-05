dc92e8ad1afad018ab7fde40853f378044e6d9ba
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:22:13 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -74,3 +74,4 @@ $(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/tou
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/berlin/berlin-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
